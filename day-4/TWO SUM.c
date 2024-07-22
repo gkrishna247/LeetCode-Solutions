07b0d5e09921fe54955d3f51fa2648e8d78fcc08
KRISHNAMOORTHI-G <krishnamoorthitech2224@gmail.com>
2024-07-22 17:04:10 +0530

    day 4

---
+++ day-4/TWO SUM.c
@@ -0,0 +1,31 @@
+#Given an array of integers and a target integer, find two numbers such that they add up to the target.
+
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+    int* result = twoSum(nums, 4, target, &returnSize);
+    printf("Indices: %d, %d\n", result[0], result[1]);
+    free(result);
+    return 0;
+}
+
+#input:  nums[] = {2, 7, 11, 15} , target = 9
